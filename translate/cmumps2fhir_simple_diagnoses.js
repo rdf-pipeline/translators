d11055306757ead7e525a68a620b2518032e99c2
Mike Carifio <mike@carif.io>
2016-05-23 16:27:28 -0400

    Implement cmumps2fhir_diagnoses in terms of cmumps2fhir_simple_diagnoses (DRY).
    Add the cmumpsDiagnosticOrder keys that don't have a mapping. Try using 'resolved_comment-100417' for 'conclusion' iff there's a value for available for this field.

--- translate/cmumps2fhir_simple_diagnoses.js
+++ translate/cmumps2fhir_simple_diagnoses.js
@@ -11,6 +11,7 @@ var fdt = require('./cmumps2fhir_datatypes');
  * @param cmumpsDiagnosisObject -- a cmumps Patient object
  * @returns {Object} -- a FHIR translation
  */
+
 function translate(cmumpsDiagnosisObject) {
     // The get function knows how to get values from cmumpsPatientObject using JSONPath.
     var get = fdt.makeGetter(cmumpsDiagnosisObject);
@@ -21,9 +22,26 @@ function translate(cmumpsDiagnosisObject) {
     // Observations (traditionally referred to as "panels" or " batteries" by laboratories) that can be used to represent relationships
     // between the individual data items."
 
+
+    // These fields have cmumps sources, but don't seem to have a map to a FHIR DiagnosticReport.
+    // get('$.acuity-100417') // Acuity, Indicate whether the acuity of this problem is currently Acute or Chronic.
+    // get('$.problem-100417') // Problem, Problem that is assigned to a patient.
+    // get('$.entering_user-100417') // Entering User, This field stores the user who made this entry to the problem list.
+    // get('$.condition-100417') // Condition, This field stores whether the problem is permanent, hidden or transcribed.
+    // get('$.modified_date-100417') // Modified Date, This field stores the date on which a modification was made to a problem.
+    // get('$.modifying_user-100417') // Modifying User, This field stores the user who made each modification to a problem.
+    // get('$.status-100417') // Status, Indicate whether this problem is currently active or inactive (resolved).
+    // get('$.number-100417') // Number, This field accepts a number between 1 and 99. (gibberish)
+    // get('$.comment-100417') // Comment, This is a word processing field.  The comments that you associate with this
+    //    problem may be as long as you need.  Once entered they can be viewed but not
+    //modified.  Additional comments may be added at any time.
+    // get('$.note-100417') // Note, this field is to allow us to associate a comment with a note.
+
+
+    var resourceType = 'DiagnosticReport';
     return fdt.clean({
-        resourceType : 'DiagnosticReport',
-        id: fdt.fhirId('DiagnosticReport', get('$._id')),
+        resourceType : resourceType,
+        id: fdt.fhirId(resourceType, get('$._id')),
         // from Resource: id, meta, implicitRules, and language
         // from DomainResource: text, contained, extension, and modifierExtension
         identifier : [ fdt.fhirIdentifierList(get('$.type')) ], // TODO: Array of Array?
@@ -44,7 +62,7 @@ function translate(cmumpsDiagnosisObject) {
         // image: [{ // Key images associated with this report
         // comment: "<string>", // Comment about the image (e.g. explanation)
         // link: { Reference(Media) } // R!  Reference to the image source
-        conclusion: get('$.diagnosis-100417'), // Clinical Interpretation of test results
+        conclusion: get('$.resolved_comment-100417') || get('$.diagnosis-100417'), // Clinical Interpretation of test results
         codedDiagnosis: fdt.fhirCodeableConcept(get('$.diagnosis-100417')), // Codes for the conclusion
         presentedForm: [cmumpsDiagnosisObject] // Entire report as issued
     });
